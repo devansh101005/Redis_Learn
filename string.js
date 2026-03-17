b2b5676a8a178496b4863bc964cd8980c1a9fd49
Devansh Pandey <devanshpandeyji4321@gmail.com>
2026-03-18 02:12:31 +0530

    leaned some ds in redis

---
+++ string.js
@@ -0,0 +1,10 @@
+const client =require("./client")
+
+async function init() {
+    await client.set("msg:6","Hey From Node.js")
+    const result = await client.get("msg:6");
+    console.log("Result =>",result);
+
+
+}
+init();
